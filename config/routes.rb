bd2927585994315a1976e3940023cf5d4c748d6d
Stuart Myers <stuart.myers@gmail.com>
2020-05-06 15:56:44 +0800

    Redo graphql addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  
+  post "/graphql", to: "graphql#execute"
   post '/graphql', to: 'graphql#execute'
-  mount_graphql_devise_for 'User', at: 'api/auth'
   mount_graphql_devise_for 'User', at: 'auth'
+
   resources :bugs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount GraphiQL::Rails::Engine,
           at: '/graphiql', graphql_path: 'graphql#execute'
   end
-  post '/graphql', to: 'graphql#execute'
 end
