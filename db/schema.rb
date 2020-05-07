ede45735aca0956d5eed6a783ba6187b771dc90c
Stuart Myers <stuart.myers@gmail.com>
2020-05-07 15:46:50 +0800

    rename bugs_collections to bug_collections

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_073346) do
+ActiveRecord::Schema.define(version: 2020_05_07_074345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bug_collections", force: :cascade do |t|
+    t.bigint "bug_id"
+    t.bigint "user_id"
+    t.boolean "collected"
+    t.boolean "donated"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bug_id"], name: "index_bug_collections_on_bug_id"
+    t.index ["user_id"], name: "index_bug_collections_on_user_id"
+  end
 
   create_table "bugs", force: :cascade do |t|
     t.string "name"
@@ -27,17 +38,6 @@ ActiveRecord::Schema.define(version: 2020_05_07_073346) do
     t.integer "internal_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "bugs_collections", force: :cascade do |t|
-    t.bigint "bug_id"
-    t.bigint "user_id"
-    t.boolean "collected"
-    t.boolean "donated"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["bug_id"], name: "index_bugs_collections_on_bug_id"
-    t.index ["user_id"], name: "index_bugs_collections_on_user_id"
   end
 
   create_table "bugs_months", force: :cascade do |t|
